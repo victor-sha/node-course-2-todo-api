a8d8c9c408426bf2260c191745e8ac50fa589995
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-04-27 16:01:05 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {ObjectID} = require('mongodb');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
-var app = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res) => {
 
-    var todo = new Todo({
+    const todo = new Todo({
         text: req.body.text
     });
     
@@ -33,7 +34,7 @@ app.get('/todos', (req, res) => {
 })
 
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
+    const id = req.params.id;
 
     if(!ObjectID.isValid(id)) {
         res.status(404).send('invalid id');
@@ -49,7 +50,7 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.delete('/todos/:id', (req, res) => {
-    var id = req.params.id;
+    const id = req.params.id;
 
     if(!ObjectID.isValid(id)) {
         res.status(404).send('invalid id');
@@ -61,6 +62,32 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     }).catch((e) => {
         res.status(400).send()
+    });
+});
+
+app.patch('/todos/:id',(req,res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text','completed']);
+
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send('invalid id');
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+      .then((todo) => {
+        if(!todo) {
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.send(400).send();
     });
 });
 
