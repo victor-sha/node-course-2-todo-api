52061aa6e9c1df200040288304de6c75f5677d76
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-04-27 18:11:29 +0300

    Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -136,3 +136,41 @@ describe('DELETE /todos/:id',() => {
             .end(done);
     });
 });
+
+describe('PATCH /todos/:id',() => {
+    it('should update the todo',(done) =>{
+        const hexId = todos[0]._id.toHexString();
+        const text = "updated text";
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed:true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(typeof res.body.todo.completedAt).toBe('number');
+            })
+            .end(done);
+    });
+
+    it('should clear completedAt when todo is not completed',(done) => {
+        const hexId = todos[1]._id.toHexString();
+        const text = "updated text 2";
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBeFalsy();
+            })
+            .end(done);
+    });
+});
