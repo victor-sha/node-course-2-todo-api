eec3825ffc30d9b23b8195f0927c5b00e5220f7e
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-04-28 18:02:09 +0300

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const { SHA256 } = require("crypto-js");
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded ',decoded);
+bcrypt.genSalt(10, (err, salt) =>{
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$1Qkys6bQ71PiHHNfwamq7eTtpxWBCar8JQUKIHDnVhwCTv7t82Wx2';
+
+bcrypt.compare(password,hashedPassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded ',decoded);
+
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
