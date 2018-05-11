6b38be278f4557dad3b41f4c5f11c64eb1ecd34f
Viktor Shaposhnikov <kassini_90@mail.ru>
2018-05-11 16:30:13 +0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -119,8 +119,17 @@ app.post('/users/login', (req, res) => {
     });
 });
 
+
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
